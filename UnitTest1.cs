2094be747518888b6ea003a7a8b0e74443f6ff81
Koronenko Anastasiia <koronenko.nastya@gmail.com>
2021-03-09 14:09:25 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Forma_Desctop_User;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject_1
+{
+    [TestClass]
+    public class Form1Test
+    {
+        [TestMethod]
+        public void WhenSetNullSetCredentialsChangedName()
+        {
+            User user = new User();
+            Assert.IsNull(user.user_Shop);
+            Assert.IsNull(user.user_Orders);
+        }
+    }
+}
