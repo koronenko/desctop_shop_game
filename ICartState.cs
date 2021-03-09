fe57f2e26fca76c2174da7f6836084dc994c2893
Koronenko Anastasiia <koronenko.nastya@gmail.com>
2021-03-09 14:11:30 +0200

    Add files via upload

---
+++ ICartState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma_Desctop_User
+{
+    partial interface ICartState
+    {
+        ICartState Transition(
+            Func<CartStateEmpty, ICartState> cartStateEmpty,
+            Func<CartStateActive, ICartState> cartStateActive,
+            Func<CartStatePaid, ICartState> cartStatePaid
+            );
+    }
+
+    class CartStateEmpty : ICartState
+    {
+        ICartState ICartState.Transition(
+            Func<CartStateEmpty, ICartState> cartStateEmpty,
+            Func<CartStateActive, ICartState> cartStateActive,
+            Func<CartStatePaid, ICartState> cartStatePaid
+            )
+        {
+            // I'm the empty state, so invoke cartStateEmpty
+            return cartStateEmpty(this);
+        }
+    }
+
+    class CartStateActive : ICartState
+    {
+        ICartState ICartState.Transition(
+            Func<CartStateEmpty, ICartState> cartStateEmpty,
+            Func<CartStateActive, ICartState> cartStateActive,
+            Func<CartStatePaid, ICartState> cartStatePaid
+            )
+        {
+            // I'm the active state, so invoke cartStateActive 		
+            return cartStateActive(this);
+        }
+    }
+
+    class CartStatePaid : ICartState
+    {
+        ICartState ICartState.Transition(
+            Func<CartStateEmpty, ICartState> cartStateEmpty,
+            Func<CartStateActive, ICartState> cartStateActive,
+            Func<CartStatePaid, ICartState> cartStatePaid
+            )
+        {
+            // I'm the paid state, so invoke cartStatePaid 		
+            return cartStatePaid(this);
+        }
+    }
+}
---
+++ Shopping_cart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma_Desctop_User
+{
+
+
+    class Shopping_cart
+    {
+
+
+        //public ICartState AddGame(ICartState currentState, Game game)
+        //{
+           // return //currentState.Transition(
+              //  cartStateEmpty => cartStateEmpty.Add(game),
+              //  cartStateActive => cartStateActive.Add(game),
+               // cartStatePaid => cartStatePaid
+             //   );
+
+       // }
+
+      //  public void Example()
+      //  {
+           // var currentState = new CartStateEmpty() as ICartState;
+
+            //add some products     
+           // currentState = AddProduct(currentState, Game.game_1);
+           // currentState = AddProduct(currentState, Game.game_2);
+
+            //pay     const decimal paidAmount = 12.34m;
+          //  currentState = currentState.Transition(
+               // cartStateEmpty => cartStateEmpty,
+               // cartStateActive => cartStateActive.Pay(paidAmount),
+              //  cartStatePaid => cartStatePaid
+              //  );
+      //  }
+    }
+}
+
